fc7cadb6a4d203fb437db0b2da3e2e2468db2686
kilonzi <mrjohnkilonzi@gmail.com>
2019-11-25 21:51:15 +0300

    Fix : Removed the node modules from Rubocop checks

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,4 +1,6 @@
+# frozen_string_literal: true
+
 class Micropost < ApplicationRecord
-    validates :content, length: {maximum: 140 },presence: true
-    belongs_to :user
+  validates :content, length: { maximum: 140 }, presence: true
+  belongs_to :user
 end
